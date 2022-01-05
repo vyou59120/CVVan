1e38dbeeecac9da981e41f6516f7dcf00434efe2
vyou59120 <van-rottana.you@hotmail.com>
2022-01-05 16:58:05 +0100

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+// var requestId;
+// var canvas = document.querySelector("#mycanvas");
+// var context = canvas.getContext("2d");
+
+// var element = document.getElementById('myCanvas');
+// var code = document.getElementById('code');
+// var icon = document.getElementsByClassName('icones');
+
+// function update() {
+//     // element.style.transform = 'rotate(90deg)';
+//     // icon[0].style.transform = 'rotate(90deg)';
+//     // context.rotate(45 * Math.PI / 180)
+//     requestId = requestAnimationFrame(update);
+// }
+
+// update();
+
+// function stopAnimation(e) {
+//     // use the requestID to cancel the requestAnimationFrame call
+//     cancelAnimationFrame(requestId);
+//     context.clearRect(0, 0, canvas.width, canvas.height);
+// }
